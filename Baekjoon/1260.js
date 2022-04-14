b29ea637eb70c4c2bd3f3ad016fd9865b763b319
dengoyoon <engus927@naver.com>
2022-04-14 17:12:36 +0900

    1260 한번 더 품

--- Baekjoon/1260.js
+++ Baekjoon/1260.js
@@ -14,12 +14,12 @@ let dfsResult = "";
 let bfsResult = "";
 
 const dfs = (node) => {
-    if (visited[node]) { 
-        return; 
+    if (visited[node]) {
+        return;
     }
     visited[node] = true;
-    dfsResult += `${node} `; 
-    graph[node].forEach( (element) => {
+    dfsResult += `${node} `;
+    graph[node].forEach( element => {
         dfs(element);
     });
 }
@@ -27,32 +27,31 @@ const dfs = (node) => {
 const bfs = (node) => {
     let queue = [node];
     visited[node] = true;
-    while (queue.length != 0) {
-        let out = queue.shift();
-        visited[out] = true;
-        bfsResult += `${out} `; 
-        graph[out].forEach( (element) => {
-            if (!visited[element]) {
+    while(queue.length != 0) {
+        let output = queue.shift();
+        bfsResult += `${output} `;
+        graph[output].forEach( element => {
+            if(!visited[element]) {
                 queue.push(element);
                 visited[element] = true;
             }
-        })
+        });
     }
 }
 
 rl.on('line', (input) => {
     if (inputCount == 2) {
-        [n, m, v] = input.split(' ').map(Number);
+        [n, m, v] = input.split(" ").map(Number);
         tempM = m;
-        graph = new Array(n+1);
-        visited = new Array(n+1).fill(false);
-        for (let i = 0 ; i < n + 1 ; i++) {
+        graph = new Array(n + 1);
+        for(let i = 0 ; i < n + 1 ; i++) {
             graph[i] = [];
         }
+        visited = new Array(n + 1).fill(false);
         inputCount -= 1;
     } else {
         tempM -= 1;
-        let [start, end] = input.split(' ').map(Number);
+        let [start, end] = input.split(" ").map(Number);
         graph[start].push(end);
         graph[end].push(start);
         if (tempM == 0) {
@@ -62,10 +61,16 @@ rl.on('line', (input) => {
 });
 
 rl.on('close', () => {
-    graph.forEach( (element) => element.sort( (a,b) => { return a-b; }));
+    graph.forEach( (element) => {
+        element.sort( (a, b) => {
+            return a - b;
+        });
+    });
+
     dfs(v);
     visited.fill(false);
     bfs(v);
+
     console.log(dfsResult);
     console.log(bfsResult);
     process.exit();
