578f2e5ea9fc8e9cf677fe74ac766fecb7bf141f
dengoyoon <engus927@naver.com>
2022-05-03 02:44:38 +0900

    내리막길 답보고 해결 ㅜ

--- Baekjoon/1520.js
+++ Baekjoon/1520.js
@@ -14,41 +14,32 @@ const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
 const dfs = (v) => {
-    if (memo[v.x][v.y] != 0) {
-        return ;
+    if (v.x == n-1 && v.y == m-1) {
+        return 1;
     }
-    const points = [];
-    let sum = 0;
+    if (memo[v.x][v.y] != -1) {
+        return memo[v.x][v.y];
+    }
+    memo[v.x][v.y] = 0;
     for (let i = 0 ; i < 4 ; i++) {
         const [nx, ny] = [v.x + dx[i], v.y + dy[i]];
         if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
             continue;
         } else {
             if (map[nx][ny] < map[v.x][v.y]) {
-                points.push({value : map[nx][ny], x : nx, y: ny});
-            } else {
-                sum += memo[nx][ny];
+                memo[v.x][v.y] += dfs({x: nx, y: ny});
             }
         }
     }
-    if (v.x == 0 && v.y == 0) {
-        memo[v.x][v.y] = 1;
-    } else {
-        memo[v.x][v.y] = sum;
-    }
-    points.sort((a, b) => {
-        return b.value - a.value;
-    })
-    points.forEach(point => {
-        dfs({x: point.x, y: point.y});
-    })
+    // console.log(memo);
+    return memo[v.x][v.y];
 }
 
 rl.on('line', (input) => {
     if (inputCount == 0) {
         [n, m] = input.split(' ').map(Number);
         tempN = n;
-        memo = Array.from(Array(n), () => Array(m).fill(0));
+        memo = Array.from(Array(n), () => Array(m).fill(-1));
         inputCount += 1;
     } else {
         tempN -= 1;
@@ -60,8 +51,6 @@ rl.on('line', (input) => {
 });
 
 rl.on('close', () => {
-    dfs({x: 0, y: 0});
-    // console.log(memo[n-1][m-1]);
-    console.log(memo);
+    console.log(dfs({x: 0, y: 0}));
     process.exit();
 });
