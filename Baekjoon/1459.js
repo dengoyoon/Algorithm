45abfeec2ba9abde502d48b9c2f7efb2347c4019
dengoyoon <engus927@naver.com>
2022-06-01 00:06:25 +0900

    걷기 해결

--- Baekjoon/1459.js
+++ Baekjoon/1459.js
@@ -13,10 +13,6 @@ rl.on('line', (input) => {
 });
 
 rl.on('close', () => {
-    // if (X == 0 || Y == 0) {
-    //     console.log(getOneSideTime(X, Y));
-    //     process.exit();
-    // }
     if (2 * side > diagonal) {
         const shorterSide = (X < Y)? X : Y;
         const result = shorterSide * diagonal + getOneSideTime(X - shorterSide, Y - shorterSide);
@@ -31,9 +27,14 @@ const getOneSideTime = (_X, _Y) => {
     if (X == 0 && Y == 0) {
         return 0;
     } else {
+        let result;
         const shortCase = (diagonal < side)? diagonal : side;
         const length = _X + _Y;
-        const result = length * shortCase + (length % 2) * side;
+        if (length % 2 == 0) {
+            result = length * shortCase;
+        } else {
+            result = (length - 1) * shortCase + side;
+        }
         return result;
     }
 }
