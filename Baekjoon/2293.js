ebdd52344a3795281fc164b8b8df2d5afee0bc13
dengoyoon <engus927@naver.com>
2022-06-03 23:24:19 +0900

    동전 1 해결중...2

--- Baekjoon/2293.js
+++ Baekjoon/2293.js
@@ -7,10 +7,26 @@ const rl = readline.createInterface({
 
 let n, k, tempN;
 const coin = [];
+let memo;
+
+const getNumberOfCase = (num) => {
+    if (num < 0) {
+        return 0;
+    }
+    if (memo[num]) {
+        return memo[num];
+    }
+    coin.forEach(value => {
+        memo[num] += getNumberOfCase(num - value);
+    });
+    return memo[num];
+}
 
 rl.on('line', (input) => {
     if (n == undefined) {
         [n, k] = input.split(' ').map(Number);
+        memo = new Array(k+1).fill(0);
+        memo[0] = 1;
         tempN = n;
     } else {
         tempN -= 1;
@@ -22,6 +38,7 @@ rl.on('line', (input) => {
 });
 
 rl.on('close', () => {
-    console.log(coin);
+    console.log(getNumberOfCase(k));
+    console.log(memo);
     process.exit();
 });
