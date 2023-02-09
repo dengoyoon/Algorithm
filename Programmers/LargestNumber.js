58e6358354874a76282e2b521d4c064aad93a5eb
dengoyoon <engus927@naver.com>
2023-02-09 20:45:40 +0900

    가장 큰 수

--- Programmers/LargestNumber.js
+++ Programmers/LargestNumber.js
@@ -1,1 +1,7 @@
-function permutation(arr, selectNum) {}
+function solution(numbers) {
+  if (numbers.reduce((a, b) => a + b) === 0) return "0";
+  return numbers
+    .map((a) => String(a))
+    .sort((a, b) => (a + b < b + a ? 1 : -1))
+    .join("");
+}
