f38ac783145abf3fc10e265e03ba20e338adbcc4
dengoyoon <engus927@naver.com>
2023-02-11 19:35:45 +0900

    게임 맵 최단거리

--- Programmers/GameMapShortestDistance.js
+++ Programmers/GameMapShortestDistance.js
@@ -2,14 +2,15 @@ const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
 const bfs = (node, maps) => {
-  let visited = Array.from(Array(5), () => Array(5).fill(0));
+  const [n, m] = [maps.length, maps[0].length];
+  let visited = Array.from(Array(n), () => Array(m).fill(0));
   const queue = [node];
   visited[node.y][node.x] = 1;
 
   for (const front of queue) {
     [0, 1, 2, 3]
       .map((i) => [front.x + dx[i], front.y + dy[i]])
-      .filter(([nx, ny]) => !(nx < 0 || nx >= 5 || ny < 0 || ny >= 5))
+      .filter(([nx, ny]) => !(nx < 0 || nx >= m || ny < 0 || ny >= n))
       .filter(([nx, ny]) => maps[ny][nx] === 1)
       .filter(
         ([nx, ny]) =>
@@ -19,10 +20,10 @@ const bfs = (node, maps) => {
       .forEach(([nx, ny]) => {
         visited[ny][nx] = visited[front.y][front.x] + 1;
         queue.push({ x: nx, y: ny });
-        if (nx === 4 && ny === 4) queue.length = 0;
+        if (nx === m - 1 && ny === n - 1) queue.length = 0;
       });
   }
-  return visited[4][4] === 0 ? -1 : visited[4][4];
+  return visited[n - 1][m - 1] === 0 ? -1 : visited[n - 1][m - 1];
 };
 
 function solution(maps) {
