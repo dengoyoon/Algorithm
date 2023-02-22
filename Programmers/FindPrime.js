0ac75546484d2ec4d6c30258b55dbb50acad19c8
dengoyoon <engus927@naver.com>
2023-02-22 12:58:15 +0900

    소수 찾기

--- Programmers/FindPrime.js
+++ Programmers/FindPrime.js
@@ -1,15 +1,33 @@
-// const bfs = (numbers) => {
-//     const queue = [...[...numbers].entries()];
-//     const visitedMap = new Map(queue.map(([_, n]) => [n, true]));
-
-//     for (const [frontIndex, front] of queue) {
-//         [...[...numbers].entries()]
-//             .filter(([index, num]) => frontIndex !== index)
-//     }
-
-//     return [...visitedMap.keys()];
-// } dfs로 바꿔야함.
+const isPrime = (number) =>
+  [...Array(Math.floor(Math.sqrt(number)) + 1).keys()]
+    .slice(2)
+    .filter((v) => number % v === 0).length === 0;
 
 function solution(numbers) {
-  console.log(bfs(numbers));
+  const numberObjects = [
+    ...[...numbers].map((value, index) => ({ value, index })),
+  ];
+
+  return new Set(
+    [...Array(numbers.length - 1).keys()]
+      .reduce(
+        (acc, _) =>
+          acc
+            .map((objArray) => [
+              objArray,
+              ...numberObjects
+                .filter(
+                  (appendObj) =>
+                    !objArray.find((obj) => obj.index === appendObj.index)
+                )
+                .map((appendObj) => [...objArray, appendObj]),
+            ])
+            .flat(),
+        [...numberObjects.map((obj) => [obj])]
+      )
+      .map((objArray) => objArray.map(({ value }) => value).join(""))
+      .map(Number)
+      .filter((n) => n > 1)
+      .filter(isPrime)
+  ).size;
 }
