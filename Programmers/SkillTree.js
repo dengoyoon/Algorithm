5e0fbca29526e10b940977c276d882581d76959a
dengoyoon <engus927@naver.com>
2023-03-22 17:37:40 +0900

    스킬트리

---
+++ Programmers/SkillTree.js
@@ -0,0 +1,25 @@
+const convertIndexArray = (skill, skillTree) =>
+  [...skill].map((sk) => [...skillTree].indexOf(sk));
+
+const isNothing = (arr) => arr.filter((a) => a !== -1).length === 0;
+
+const isPrior = (arr) => {
+  const findLength = arr.filter((a) => a !== -1).length;
+  if (arr.length === findLength) return true;
+  return arr.findIndex((a) => a === -1) >= findLength;
+};
+
+const isAscendingOrder = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] > arr[i]) return false;
+  }
+  return true;
+};
+
+function solution(skill, skillTrees) {
+  return skillTrees
+    .map((skillTree) => convertIndexArray(skill, skillTree))
+    .filter((arr) => isPrior(arr))
+    .map((arr) => arr.filter((a) => a !== -1))
+    .filter((arr) => isAscendingOrder(arr)).length;
+}
